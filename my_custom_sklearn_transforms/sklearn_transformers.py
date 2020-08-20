a08773c9bb9050643d4af04cf73b83c509c5c6f4
Sebastian <anse23@hotmail.com>
2020-08-20 18:19:44 -0500

    feat: Added scaler transform

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler
+
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -8,9 +10,38 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
         return data.drop(labels=self.columns, axis='columns')
+    
+class ScaleColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, column_names_avoid):
+        self.column_names_avoid = column_names_avoid
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        tr = StandardScaler(copy=True)
+        
+        data = X.copy()
+        
+        data_1 = data.drop(self.column_names_avoid, axis=1)
+        
+        tr_transformer = tr.fit(X=data_1)
+        
+        data_2 = tr_transformer.transform(X=data_1)
+        
+        data_3 = pd.DataFrame.from_records(
+            data=data_2,
+            columns=data_1.columns
+        )
+        
+        data_4 = data[self.column_names_avoid]
+        
+        data_5 = data_4.join(data_3)
+        
+        return data_5
